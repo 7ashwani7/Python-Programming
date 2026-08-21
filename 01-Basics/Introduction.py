63d83a5f0c1d23cc0f116238df3fdfd8c6b9ed32
7ashwani7 <chauhanashwani548@gmail.com>
2026-08-21 21:28:51 +0530

    .

---
+++ 01-Basics/Introduction.py
@@ -0,0 +1,49 @@
+# 1. Hello World
+
+print("Hello World")
+
+
+# 2. Multiple print statements
+
+print("Hello")
+print("Python")
+
+
+# 3. Printing numbers
+
+print(100)
+print(10 + 20)
+
+
+# 4. Printing multiple values
+
+print("Name:", "Ashwani")
+print("Age:", 20)
+
+
+# 5. Comments
+
+# This is a Python comment
+
+print("Comments are ignored by Python")
+
+
+# 6. Case sensitivity
+
+name = "Ashwani"
+Name = "Rahul"
+
+print(name)
+print(Name)
+
+
+# 7. Indentation
+
+if True:
+    print("Python uses indentation")
+
+
+# 8. No semicolon required
+
+x = 10
+print(x)
---
+++ 01-Basics/Variables.py
@@ -0,0 +1,149 @@
+# Python Variables
+
+
+# 1. Variable Declaration
+
+age = 20
+
+print(age)
+
+
+# 2. Variable with String
+
+name = "Ashwani"
+
+print(name)
+
+
+# 3. Variable with Float
+
+price = 99.99
+
+print(price)
+
+
+# 4. Variable with Boolean
+
+is_student = True
+
+print(is_student)
+
+
+# 5. Checking Variable Type
+
+age = 20
+
+print(type(age))
+
+
+# 6. Dynamic Typing
+
+x = 10
+
+print(x)
+print(type(x))
+
+x = "Hello"
+
+print(x)
+print(type(x))
+
+
+# 7. Multiple Variables
+
+a = 10
+b = 20
+c = 30
+
+print(a)
+print(b)
+print(c)
+
+
+# 8. Multiple Assignment
+
+a, b, c = 10, 20, 30
+
+print(a)
+print(b)
+print(c)
+
+
+# 9. Same Value to Multiple Variables
+
+a = b = c = 100
+
+print(a)
+print(b)
+print(c)
+
+
+# 10. Swapping Two Variables
+
+a = 10
+b = 20
+
+a, b = b, a
+
+print(a)
+print(b)
+
+
+# 11. Valid Variable Names
+
+name = "Ashwani"
+age = 20
+student_name = "Ashwani"
+marks2 = 90
+
+print(name)
+print(age)
+print(student_name)
+print(marks2)
+
+
+# 12. Invalid Variable Names
+
+# 2name = "Ashwani"
+# student-name = "Ashwani"
+# class = 10
+
+
+# 13. Copying a Variable
+
+a = 100
+b = a
+
+print(a)
+print(b)
+
+
+# 14. Reassigning a Variable
+
+x = 10
+
+print(x)
+
+x = 50
+
+print(x)
+
+
+# 15. Constants Convention
+
+PI = 3.14159
+MAX_SIZE = 100
+
+print(PI)
+print(MAX_SIZE)
+
+
+# 16. Snake Case
+
+first_name = "Ashwani"
+last_name = "Chauhan"
+total_marks = 450
+
+print(first_name)
+print(last_name)
+print(total_marks)
